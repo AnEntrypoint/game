cae912c98f35c730f30de20dbb5da0751899c3a4
jb0gie <up620161@myport.ac.uk>
2021-11-29 00:51:29 +0000

    can you see dis

--- HealthBar/HealthBar2D.gd
+++ HealthBar/HealthBar2D.gd
@@ -8,16 +8,9 @@ var bar_yellow = preload("res://HealthBar/barHorizontal_yellow_mid 200.png")
 #	hide()
 	
 func update_bar(amount, full):
-	if amount < full:
-		show()
 	texture_progress = bar_green
 	if amount < 0.75 * full:
 		texture_progress = bar_yellow
 	if value < 0.45 * full:
 		texture_progress = bar_red
 	value = amount
-
-func _on_HealthBar2D_gui_input(event):
-	if event is InputEventMouseButton and event.pressed:
-		show()
-		update_bar(value-1, 10)
